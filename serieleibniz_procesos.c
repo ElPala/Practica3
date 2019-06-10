ff4afa3281274de9e511b38966d0fb789329baf8
Elpala <luis.pala1997@gmail.com>
2019-06-09 23:11:25 -0600

    Finised sere lebniz only ussing proces, implementig the function shget shmat smdt to share memory

--- serieleibniz_procesos.c
+++ serieleibniz_procesos.c
@@ -12,7 +12,7 @@ void proc(int n,int id );
 int main(int argc, char const *argv[]) {
   pid_t p;
   int status;
-  int id;
+  int key;
   int i;
   double *  pleibniznumber;
   long long start;
@@ -21,29 +21,34 @@ int main(int argc, char const *argv[]) {
   struct timeval ts;
   gettimeofday(&ts,NULL);
   start = ts.tv_sec;
-  id = shmget(IPC_PRIVATE, sizeof(double),IPC_CREAT | 0666 );
-  if(id<0){
+  key = shmget(IPC_PRIVATE, sizeof(double), IPC_CREAT | 0666);
+  if(key<0){
+    printf("Erron en la creación de la memoria compartida\n");
     exit(1);
   }
-
+//printf("%d %d 1\n",id, getpid());
   for ( i = 0; i < NPROCS; i++) {
     p=fork();
   	if(p==0){
-      proc(i, id);
+      proc(i, key);
+      exit(0);
     }
   }
 
   for ( i = 0; i < NPROCS; i++)
       wait(&status);
-  pleibniznumber = (double*) shmat(id, NULL, 0);
+  //printf("%d %d 3\n",id, getpid());
+  pleibniznumber = (double*) shmat(key, NULL, 0);
   if (pleibniznumber == (void *) -1) {
+    printf("Erron en ABRIR puntero 1 %d\n",getpid());
     exit(1);
   }
-
   printf("%f\n", *pleibniznumber );
-  if (shmdt(pleibniznumber) == -1)
-      exit(1);
 
+  if (shmdt(pleibniznumber) == -1){
+    printf("Erron en CERRAR puntero 1 %d\n",getpid());
+        exit(1);
+  }
   gettimeofday(&ts,NULL);
   finish = ts.tv_sec;
   time = finish-start;
@@ -51,15 +56,17 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-void proc(int n, int id )
+void proc(int n, int key )
 {
   int inicio;
 	int fin;
   int j;
   double sign;
   double*  pleibniznumber;
-  pleibniznumber = (double*)shmat(id, NULL, 0);
+  //printf("%d %d 2\n",id, getpid());
+  pleibniznumber = (double*)shmat(key, NULL, 0);
   if (pleibniznumber == (void *) -1) {
+    printf("Erron en ABRIR puntero 0 %d\n",getpid());
     exit(1);
   }
   inicio = n*(CYCLES/NPROCS);
@@ -69,16 +76,15 @@ void proc(int n, int id )
   } else {
     sign=-1;
   }
-  printf("soy hijo %d %d-%d\n", getpid(),inicio, fin );
+
+  //printf("numero- %f %d 1\n", *pleibniznumber , getpid());
+  //printf("soy hijo %d %d-%d\n", getpid(),inicio, fin );
   for (j = inicio; j < fin; j++) {
-    (*pleibniznumber)+= sign/((2*n) + 1);
+    (*pleibniznumber)+= sign/((2*j) + 1);
     sign *= -1;
   }
-
-  if (shmdt(pleibniznumber) == -1){
+  //printf("numero:%f %d 2\n", *pleibniznumber , getpid());
+  if (shmdt(pleibniznumber) == -1)
+      printf("Erron en CERRAR puntero 0 %d\n",getpid());
       exit(1);
-  }
-  else{
-    exit;
-  }
 }
