4530029d3a6eb13377b4f8870f10e571be60a7e3
Tu Nombre <alex-mora95@hotmail.com>
2019-06-10 18:04:53 -0500

    nada

--- serieleibniz_hilosprocesos.c
+++ serieleibniz_hilosprocesos.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/time.h>#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/wait.h>
 #define CYCLES 2000000000
 
 int main(int argc, char const *argv[]) {
-  void fn();
+
   int i;
   double leibniznumber=0;
   double sign = -1;
@@ -14,11 +15,21 @@ int main(int argc, char const *argv[]) {
   long long finish;
   long long time;
   struct timeval ts;
+  void fn(void *args);
+
+
   gettimeofday(&ts,NULL);
   start = ts.tv_sec;
 
+int parArr[NTHREADS];
+for(i=0;i<NTHREADS;i++)
+{
+  parArr[i]=i;
+}
+
+
   void *pchild_stack = malloc(1024 * 1024);
-  int pid = clone(fn, pchild_stack + (1024 * 1024),SIGCHLD, &argv);
+  int pid = clone(fn, pchild_stack + (1024 * 1024),SIGCHLD, );
 
 
   printf("%f\n",leibniznumber );
