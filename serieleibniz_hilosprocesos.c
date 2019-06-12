996e1d42e0bed4a950007fb5205b54a5a31b1f79
Your Name <alex-mora95@hotmail.com>
2019-06-11 22:37:41 -0600

    test

--- serieleibniz_hilosprocesos.c
+++ serieleibniz_hilosprocesos.c
@@ -1,13 +1,16 @@
 #include <sched.h>
+#include <linux/sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/wait.h>
+#include <string.h>
+#define _GNU_SOURCE
 #define CYCLES 2000000000
 
 int main(int argc, char const *argv[]) {
-  int clone(void *, void *, int ,int,double,double);
+  //int clone(int (*pfn)(void), void *, int ,int,double,double);
   int i;
   double leibniznumber=0;
   double sign = -1;
@@ -15,13 +18,44 @@ int main(int argc, char const *argv[]) {
   long long finish;
   long long time;
   struct timeval ts;
-  void fn(int, double, double);
+
+  //void fn(int, double, double);
+
+  const int STACK_SIZE = 65536;
+  char* stack = malloc(STACK_SIZE);
+  if (!stack) {
+    perror("malloc");
+      exit(1);
+      }
+
+  unsigned long flags = 0;
+  if (argc > 1 && !strcmp(argv[1], "vm")) {
+        flags |= CLONE_VM;
+       }
+
+   char buf[100];
+   strcpy(buf, "hello from parent");
+   if (clone(child_func, stack + STACK_SIZE,
+     flags | SIGCHLD, buf) == -1) {
+      perror("clone");
+          exit(1);
+        }
+
+  int status;
+  if (wait(&status) == -1) {
+        perror("wait");
+            exit(1);
+          }
+           printf("Child exited with status %d. buf =\"%s\"\n", status, buf);
+            return 0;
+          }
+
 
   gettimeofday(&ts,NULL);
   start = ts.tv_sec;
 
-  void *pchild_stack = malloc(1024 * 1024);
-  int pid = clone(fn, pchild_stack + (1024 * 1024),SIGCHLD,i, leibniznumber,sign );
+  //void *pchild_stack = malloc(1024 * 1024);
+  //int pid = clone(fn, pchild_stack + (1024 * 1024),SIGCHLD,i, leibniznumber,sign );
   //fn(i, leibniznumber, sign);
 
   printf("%f\n",leibniznumber );
@@ -30,15 +64,20 @@ int main(int argc, char const *argv[]) {
   time = finish-start;
   printf("Tiempo total: %d seg\n",(int) time );
   return 0;
-
-  //free(pchild_stack);
-
-
 }
+
   void fn(int i, double leibniznumber, double sign){
+    printf("si lleguew");
 
     for (i = 0; i < CYCLES; i++) {
       sign *= -1;
       leibniznumber+= sign/((2*i) + 1);
     }
+}
+
+static int child_func(void* arg) {
+  char* buf = (char*)arg;
+  printf("Child sees buf = \"%s\"\n", buf);
+ strcpy(buf, "hello from child");
+ return 0;
 }
